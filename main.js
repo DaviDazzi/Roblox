d8d05eeb356ffbfd5a14d8a5ee592cb3cdda78f9
DaviDazzi <129235807+DaviDazzi@users.noreply.github.com>
2023-03-28 21:09:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function BloxFruit(){
+    window.location="BloxFruits.html";
+}
+function ShindoLife(){
+    window.location="ShindoLife.html";
+}
+function SurviveArea51(){
+    window.location="SurviveArea51.html";
+}
+function Welcome(){
+    window.location="index.html";
+}
